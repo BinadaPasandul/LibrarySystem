7d6340910a3068e73cb0ec45b495837187531462
BinadaPasandul <binadapasandul@gmail.com>
2025-12-05 19:07:16 +0530

    Initial backend setup with .NET API and SQLitw

---
+++ backend/Models/Book.cs
@@ -0,0 +1,10 @@
+namespace backend.Models
+{
+    public class Book
+    {
+        public int Id { get; set;} //primary key
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
